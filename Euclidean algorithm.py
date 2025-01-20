dfa0f0cd2ca63292dc62045440d73c4c1e7eaa2a
wanghyuk <wanghyuk2024@163.com>
2025-01-21 05:25:30 +0800

    Update Euclidean algorithm.py
    can accept input numbers via keyboard inputs and give the user an error message if the input is invalid.

--- Euclidean algorithm.py
+++ Euclidean algorithm.py
@@ -11,9 +11,26 @@ class Euclidean_Algorithm:
             #caculate
             self.a , self.b = self.b , self.a % self.b
         return self.a
-    
-#caculate the gcd
-caculate = Euclidean_Algorithm(270,192)
-gcd = caculate.GCD()
-print(f"The greatest common divisor 270 of 192 and is {gcd}")
 
+#check if the number is available
+def available_number(num):
+    #try if the num can be converted to integer
+    try:
+        integer = int(num)
+        return isinstance(integer,int) and integer >= 0
+    #if the number can not be converted, return false
+    except ValueError:
+        return False
+    
+#user input the number
+num1 = input("Please input the first number")
+num2 = input("please input the second number")
+if available_number(num1) and available_number(num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    #caculate the gcd
+    caculate = Euclidean_Algorithm(num1, num2)
+    gcd = caculate.GCD()
+    print(f"The greatest common divisor of {num1} and {num2} is {gcd}")
+else:
+    print("Please enter positive integer")
